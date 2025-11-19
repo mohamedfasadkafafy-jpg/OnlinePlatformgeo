45d9c4b218b789d2742fed805dad220944ee9589
mohamedfasadkafafy-jpg <mohamedfasadkafafy@gmail.com>
2025-11-19 23:31:31 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,25 @@
+document.getElementById('loginForm').addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const username = document.getElementById('username').value.trim();
+    const password = document.getElementById('password').value.trim();
+    const errorMsg = document.getElementById('errorMsg');
+
+    // ALL USERS IN ONE ARRAY
+    const users = [
+        {username: "Aya Medhat", password: "Am54", link: "https://mohamedfasadkafafy-jpg.github.io/OnlinePlatformayam/"},
+    ];
+
+    // FIND USER
+    const validUser = users.find(user =>
+        user.username.toLowerCase() === username.toLowerCase() &&
+        user.password === password
+    );
+
+    if(validUser){
+        window.location.href = validUser.link;
+    } else {
+        errorMsg.textContent = "Could not find your account";
+    }
+});
+
